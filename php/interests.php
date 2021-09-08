613b8a19673466d700ad8c1440f01acc392ccf66
Rick van Wijk <404350@student.fontys.nl>
2021-09-08 15:31:46 +0200

    added minor folder structure

---
+++ php/interests.php
@@ -0,0 +1,213 @@
+<!------------------------------------------INTERESTS-------------------------------------------------------------------------------------->
+<div class="tab" id="interests">
+                <h1 class="home-base-title">Interests</h1>
+                <p class="home-base-p">A few of my spare time projects.</p>
+                <div class="row grid-holder">
+                    <!------------------------------------------VFX editing-------------------------------------------------------------------------------------->
+                    <div id="overview-grid" class="overview-grid" data-modal="hp"
+                        style="background-image: url('assets/images/hp.jpg');">
+                        <div class="a-ghostbtn">
+                            <a class="a-ghostbtn">
+                                <div id="hover-box" class="hover-box" style="background-color:rgba(0, 0, 0, .4);">
+                                    <div class="ghostbutton">VFX editing<span class="fa fa-external-link-alt"></span>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+
+
+                    <div id="hp" class="modal">
+                        <div class="modal-dialog modal-lg"></div>
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <a id="modal-close" class="modal-close" data-dismiss="modal">×</a>
+                                <div class="modal-title"><img src="assets/images/hpTitle.png" width="40%" alt=""></div>
+                            </div>
+                            <div class="modalbody-container">
+                                <br>
+                                <div class="col-lg-8 mx-auto">
+                                    <i>The movie series 'Harry Potter' is my favourite movie series and since I like
+                                        video editing I decided to recreate some of the most known Harry Potter effects.
+                                    </i>
+
+
+
+                                </div>
+                                <br><br><br><br>
+                                <div class="col-lg-8 mx-auto">
+                                    <div class="modal-body">
+                                        <br><br>
+                                        <i>The 'Harry Potter intro' effect</i>
+                                        <br><br>
+                                        <video style="border-style: none;" width="100%" height="auto" controls=""
+                                            src="assets/vids/HarryPotterWithFullTitle.mp4" data-video="0"></video>
+                                        <br><br><br><br>
+                                        <i>The 'Lumos' effect</i>
+                                        <br><br>
+                                        <video style="border-style: none;" width="100%" height="auto" controls=""
+                                            src="assets/vids/LumosWithVoiceOver.mp4" data-video="0"></video>
+                                        <br><br><br><br>
+                                        <i>The 'Expecto Patronum' effect</i>
+                                        <br><br>
+                                        <video controls muted style="border-style: none;" width="100%" height="auto"
+                                            controls="" src="assets/vids/ExpectoPatronummm.mp4" data-video="0"></video>
+                                        <br><br>
+                                        <hr><br><br>
+                                        <i style="font-size: 150%;">The Result</i>
+                                        <br><br>
+                                        <div class="iframe-container">
+                                            <iframe class="video" src="https://www.youtube.com/embed/L2-a06F4e44"
+                                                frameborder="0"
+                                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                                allowfullscreen>
+                                            </iframe>
+                                        </div>
+                                        <br>
+                                        <br>
+                                        <br>
+                                        <br>
+                                        <br>
+                                        <a target="_blank" href="sprintx.html">
+                                            <div class="paddedText">
+                                                <p>I made a simple tutorial on the 'Lumos' effect (this was made for
+                                                    'TheLeakyCauldron.org') so if you are a big Harry
+                                                    Potter/Wizarding World fan like me feel free to try it yourself.</p>
+                                                <br>
+
+                                                <span class="fa fa-external-link-alt"></span>
+                                            </div>
+                                        </a>
+                                        <br>
+                                    </div>
+                                </div>
+
+                                <div class="col-lg-8 mx-auto">
+
+                                </div>
+                            </div>
+                            <div class="modal-footer">
+                                <button id="btnModalClose" class="btnModalClose" data-dismiss="modal">Close</button>
+                            </div>
+                        </div>
+                    </div>
+                    <!------------------------------------------p5-------------------------------------------------------------------------------------->
+                    <div id="overview-grid" class="overview-grid" data-modal="p5login"
+                        style="background-image: url('assets/images/p5.jpeg');">
+                        <div class="a-ghostbtn">
+                            <a class="a-ghostbtn">
+                                <div id="hover-box" class="hover-box">
+                                    <div class="ghostbutton">p5<span class="fa fa-external-link-alt"></span></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div id="p5login" class="modal">
+                        <div class="modal-dialog modal-lg"></div>
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <a id="modal-close" class="modal-close">×</a>
+                                <div class="modal-title"><img src="assets/images/p5.png" width="30%" alt=""></div>
+                            </div>
+                            <div class="modalbody-container">
+                                <br>
+                                <div class="col-lg-8 mx-auto">
+                                    <i>p5 is a cool JavaScript library used for web art.
+                                    </i>
+                                    <br><br><br>
+                                    <img src="assets/images/p5_tile.png" width="100%" alt="">
+                                    <br>
+                                    <italic>Made with p5</italic>
+                                    <br><br><br>
+                                    <div class="paddedText">
+                                        <p>
+                                            p5 can be a useful skill to learn for web design since instead of using a
+                                            static image you
+                                            can use p5 to create dynamic elements for the design of your website. This
+                                            gives you the chance
+                                            of creating beautiful, interactive user experiences.
+                                        </p>
+                                        <div class="paddedTextLine"></div>
+                                    </div>
+
+
+                                </div>
+                                <br><br><br><br>
+                                <div class="col-lg-8 mx-auto">
+                                    <div class="modal-body">
+
+                                        <p>With p5 I created the login page below.</p>
+                                        <br>
+                                        <video width="100%" height="340" controls="" src="assets/vids/p5.mp4"
+                                            data-video="0"></video>
+
+
+                                        <br>
+                                        <br>
+                                        <br>
+                                        <a target="_blank" href="https://github.com/RickvWijk1/p5_Login">
+                                            <div class="paddedText">
+                                                <p>View the repo for more information here</p>
+                                                <br>
+
+                                                <img src="assets/icons/github.svg" style="width: 10%;" alt="Github">
+                                            </div>
+                                        </a>
+                                        <br>
+
+                                    </div>
+                                </div>
+
+                                <div class="col-lg-8 mx-auto">
+
+                                </div>
+                            </div>
+                            <div class="modal-footer">
+                                <button id="btnModalClose" class="btnModalClose">Close</button>
+                            </div>
+                        </div>
+                    </div>
+                    <!------------------------------------------Pixel Art-------------------------------------------------------------------------------------->
+                    <div id="overview-grid" class="overview-grid" data-modal="pixelart"
+                        style="background-image: url('assets/images/pixelart/AppleCiderPixel.png')">
+                        <div class="a-ghostbtn">
+                            <a class="a-ghostbtn">
+                                <div id="hover-box" class="hover-box">
+                                    <div class="ghostbutton">Pixel Art<span class="fa fa-external-link-alt"></span>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <div id="pixelart" class="modal">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <a id="modal-close" class="modal-close">×</a>
+                                <!-- <div class="modal-title"><img src="assets/images/p5.png" width="30%" alt=""></div> -->
+                            </div>
+                            <div class="modalbody-container">
+                                <br>
+                                <div class="col-lg-8 mx-auto">
+                                    <i>Project under construction!
+                                    </i>
+                                    <br><br><br>
+                                    <svg role="img" class="constrPlaceh">
+                                        <use xlink:href="assets/images/stage/constr_placeh.svg#constr_placeh"
+                                            href="assets/images/stage/constr_placeh.svg#constr_placeh"></use>
+                                    </svg>
+                                </div>
+                                <br><br><br><br>
+                                <div class="col-lg-8 mx-auto">
+                                    <div class="modal-body">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="modal-footer">
+                                <button id="btnModalClose" class="btnModalClose">Close</button>
+                            </div>
+                        </div>
+                    </div>
+
+
+                </div> <!-- end of grid -->
+            </div>

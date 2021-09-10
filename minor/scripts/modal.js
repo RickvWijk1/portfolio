549f4b5a8ff397dc6bef163abaa1e227f8a58542
Rick van Wijk <404350@student.fontys.nl>
2021-09-10 13:58:55 +0200

    added new filter system with design

--- minor/scripts/modal.js
+++ minor/scripts/modal.js
@@ -1,5 +1,5 @@
 //element that triggers the modal to open
-let openModal = document.querySelectorAll('#overview-grid, #stage_assignment');
+let openModal = document.querySelectorAll('#overview-grid');
 // document.getElementsByTagName("BODY").style.overflowY = "auto";
 //each modal opens the moment the trigger elemnent gets clicked.
 openModal.forEach((trigger) => {
@@ -12,21 +12,6 @@ openModal.forEach((trigger) => {
         document.getElementById(modal).style.overflowY = "auto";
 
         document.getElementsByTagName("HTML")[0].style.overflowY="hidden";
-
-        //necessary to show a different slide for each modal
-        if(document.querySelector('#assignorient').style.visibility == "visible"){
-            document.querySelector('#mySwipe').style.display="block";
-            document.querySelector('#mySwipe').style.visibility="visible";
-        }
-        //necessary to show a different slide for each modal
-        if(document.querySelector('#intview_design').style.visibility == "visible"){
-            document.querySelector('#mySwipeDesign').style.display="block";
-            document.querySelector('#mySwipeDesign').style.visibility="visible";
-        }
-        if(document.querySelector('#intview_design').style.visibility == "visible"){
-            document.querySelector('#mySwipePersona').style.display="block";
-            document.querySelector('#mySwipePersona').style.visibility="visible";
-        }
     };
     
 });
@@ -40,12 +25,6 @@ closeModal.forEach((trigger) => {
 
         //the modal becomes invisible
         modal = trigger.closest('.modal').style.visibility = "hidden";
-        document.getElementById('mySwipe').style.visibility = "hidden";
-        document.getElementById('mySwipe').style.display = "none";
-        document.getElementById('mySwipeDesign').style.visibility = "hidden";
-        document.getElementById('mySwipeDesign').style.display = "none";
-        document.getElementById('mySwipePersona').style.visibility = "hidden";
-        document.getElementById('mySwipePersona').style.display = "none";
         document.getElementsByTagName("HTML")[0].style.overflowY="auto";
     };
 });
@@ -54,66 +33,17 @@ closeModal.forEach((trigger) => {
 window.onclick = (e) => {
     if (e.target.className === 'modal') {
         e.target.style.visibility = "hidden";
-        document.getElementById('mySwipe').style.visibility = "hidden";
-        document.getElementById('mySwipe').style.display = "none";
-        document.getElementById('mySwipeDesign').style.visibility = "hidden";
-        document.getElementById('mySwipeDesign').style.display = "none";
-        document.getElementById('mySwipePersona').style.visibility = "hidden";
-        document.getElementById('mySwipePersona').style.display = "none";
         document.getElementsByTagName("HTML")[0].style.overflowY="auto";
     }
 };
 
-
-
 // close modal on 'ESC' key press
 window.addEventListener('keydown', e => {
     if (e.key === "Escape") {
-        //Stage page
-        document.querySelector('#internintro').style.visibility = "hidden";
-        document.querySelector('#assignorient').style.visibility = "hidden";
-        document.querySelector('#intview_design').style.visibility = "hidden";
-        document.querySelector('#design_testing').style.visibility = "hidden";
-        document.querySelector('#realisation').style.visibility = "hidden";
-        document.querySelector('#mySwipe').style.visibility = "hidden";
-        document.querySelector('#mySwipeDesign').style.visibility = "hidden";
-        document.querySelector('#mySwipePersona').style.visibility = "hidden";
-
         document.getElementsByTagName("HTML")[0].style.overflowY="auto";
-        // document.querySelectorAll('.modal').style.display = "none";
     }
 });
 
 //routing
 window.onload = (event) => {
-    if(location.hash==='#modal-intro'){
-        document.getElementById('internintro').style.display = "block";
-        document.getElementById('internintro').style.visibility = "visible";
-        document.getElementsByTagName("HTML")[0].style.overflowY="hidden";
-        document.getElementById('internintro').style.overflowY = "auto";
-    }
-    else if(location.hash==='#modal-assign'){
-        document.getElementById('assignorient').style.display = "block";
-        document.getElementById('assignorient').style.visibility = "visible";
-        document.getElementsByTagName("HTML")[0].style.overflowY="hidden";
-        document.getElementById('assignorient').style.overflowY = "auto";
-    }
-    else if(location.hash==='#modal-inter'){
-        document.getElementById('intview_design').style.display = "block";
-        document.getElementById('intview_design').style.visibility = "visible";
-        document.getElementsByTagName("HTML")[0].style.overflowY="hidden";
-        document.getElementById('intview_design').style.overflowY = "auto";
-    }
-    else if(location.hash==='#modal-design'){
-        document.getElementById('design_testing').style.display = "block";
-        document.getElementById('design_testing').style.visibility = "visible";
-        document.getElementsByTagName("HTML")[0].style.overflowY="hidden";
-        document.getElementById('design_testing').style.overflowY = "auto";
-    }
-    else if(location.hash==='#modal-reali'){
-        document.getElementById('realisation').style.display = "block";
-        document.getElementById('realisation').style.visibility = "visible";
-        document.getElementsByTagName("HTML")[0].style.overflowY="hidden";
-        document.getElementById('realisation').style.overflowY = "auto";
-    }
 };

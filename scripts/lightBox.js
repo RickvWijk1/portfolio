fc5b8659856805e0182fbcb92a38bd509f778d2b
Rick van Wijk <404350@student.fontys.nl>
2021-12-25 14:19:54 +0100

    added content and slider.js

--- scripts/lightBox.js
+++ scripts/lightBox.js
@@ -2,7 +2,7 @@ const lightbox = document.createElement('div')
 lightbox.id = 'lightbox'
 document.body.appendChild(lightbox)
 
-const images = document.querySelectorAll('.modalbody-container img')
+const images = document.querySelectorAll('.image-zoom')
 images.forEach(image => {
   image.addEventListener('click', e => {
     lightbox.classList.add('active')
